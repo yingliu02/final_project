77800830984433a2544ed818c1c3bcc186626515
yingliu02 <150416891+yingliu02@users.noreply.github.com>
2024-10-30 20:39:38 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,36 +1,23 @@
 import streamlit as st
+from langchain_openai.chat_models import ChatOpenAI
 
-import agent
+st.title("🦜🔗 Quickstart App")
 
-st.title("Seinfeld Bot")
-model_name = st.radio("Select a model", ["gpt4o", "llama3.1"])
+openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+def generate_response(input_text):
+    model = ChatOpenAI(temperature=0.7, api_key=openai_api_key)
+    st.info(model.invoke(input_text))
 
-# Load the model
-model = agent.load_model(model_name)
 
-# React to user input
-if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
-    st.chat_message("user").markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
-    response = f"{model_name}: {model(prompt)}"  # model(prompt)
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        st.markdown(response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
-
-cleared = st.button("Clear chat history")
-if cleared:
-    st.session_state.messages = []
+with st.form("my_form"):
+    text = st.text_area(
+        "Enter text:",
+        "What are the three key pieces of advice for learning how to code?",
+    )
+    submitted = st.form_submit_button("Submit")
+    if not openai_api_key.startswith("sk-"):
+        st.warning("Please enter your OpenAI API key!", icon="⚠")
+    if submitted and openai_api_key.startswith("sk-"):
+        generate_response(text)
